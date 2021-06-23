b47387e62121597b0540f0c2ae221b278083f36e
Elise Easter Christiansen <eec@dips.no>
2021-06-23 16:24:17 +0200

    some changes

--- src/AddPatientForm.svelte
+++ src/AddPatientForm.svelte
@@ -1,64 +1,39 @@
 <script>
 
-	let patients = [
-		{Name: "Åge", Age: 24, Gender: "Male", id: 0},
-		{Name: "Sara", Age: 29, Gender: "Female", id: 1}
-	]
+	export let patientName;
+	export let patientId;
+	let code;
+	let comment;
 
-	let patientName;
-	let age;
-	let gender = 0;
-
-	export let showForm;
-
-	const addPatient = () => {
-        showForm = !showForm
-		let g = "Male";
-		{if (gender==1) {
-			g = "Female";
-		}}
-		
-		let newPatient = {Name: patientName, Age: age, Gender: g, id: patients.length};
-		patients[patients.length] = newPatient; 
-		console.log(newPatient);
-		resetFields();
-        
-        console.log(showForm)
+	const saveDocument = () => {
+		let document = {Name: patientName, PatientId: patientId, Comment: comment};
+		let documentString = JSON.stringify(document)
+		console.log(documentString);
+		//window.chrome.webview.postMessage()
+		window.chrome.webview.postMessage(documentString);
 	}
 
-	const resetFields = () => {
-		patientName = "";
-		age = "";
-		gender = 0;
-
+	const approveDocument = () => {
+		let document = {Name: patientName, PatientId: patientId, Comment: comment};
+		let documentString = JSON.stringify(document)
+		console.log("Approved:" + documentString);
 	}
 
 </script>
 
 <main>
-	{#if showForm}
-		<form on:submit|preventDefault={addPatient}>
-			<input type= "text" placeholder="Name" bind:value={patientName} required/>
-			<input type= "number" placeholder="Age" bind:value={age} required/>
-			<label>
-				<input type= "radio" bind:group={gender} value={0} required/>
-				Male
-			</label>
-			<label>
-				<input type= "radio" bind:group={gender} value={1} required/>
-				Female
-			</label>
-			<button>Add patient</button>
-		</form>
-		
-	{/if}
+	
+		<!-- <form on:submit|preventDefault={addPatient}> -->
+			<!-- <input type= "number" placeholder="Sykemelding kode" bind:value={code} required/> -->
 
-    	<h2>Patient List</h2>
-	{#each patients as patient (patient.id)}
-		<div>
-			<h3>{patient.Name}</h3>
-		</div>
-	{/each}
+				<input type= "text" placeholder="Kommentar" bind:value={comment} required/>
+
+				<button name="saveButton" type="submit" on:click={saveDocument}>Lagre</button>
+				<button name="approveButton" type="submit" on:click={approveDocument}>Godkjenn</button>
+
+			
+		<!-- </form> -->
+	
 </main>
 
 <style>
--- src/App.svelte
+++ src/App.svelte
@@ -2,29 +2,65 @@
 
 import Modal from './Modal.svelte'
 import AddPatientForm from './AddPatientForm.svelte'
+import { get } from 'svelte/store';
 let showModal = false;
-let showForm = false;
+// let showForm = false;
 
+let patientName = "Andreas";
+let patientId = "55555";
+
+	window.chrome.webview.addEventListener('message', event => alertSomething(event.data));
 
 // Modal
 const toggleModal = () => {
 	showModal = !showModal;
 }
 
-// Form
-export const ToggleForm = () => {
-		showForm = !showForm;
-		console.log(showForm)
+const alertSomething = (e) => {
+	console.log(e);
+	if(e == "heihei"){
+		alert(e);
+		saveDoc();
+	}else if(e == "Elise"){
+		patientName = e;
+		console.log(patientName)
+		alert(e)
 	}
+	else {
+		alert("Not saving");
+	}
+	
+}
+
+const saveDoc = () => {
+	alert("saving document");
+	//window.chrome.webview.postMessage("Melding fra svelte-app");
+}
+
+// function alertSomething(){
+// 	alert("elise");
+// }
+
+//window.addEventListener('load', alertSomething);
+
+
+
 
 </script>
 
 <Modal header="Information" message="Here is some information" isInfo={true} {showModal} on:click={toggleModal}/>
 <main>
-	<button on:click={toggleModal}>Open modal</button>
-	<button on:click={ToggleForm}>Add new patient</button>
-	<AddPatientForm {showForm} on:click={ToggleForm}/>
-
+	<button on:click={toggleModal}>?</button>
+	<div>
+		<label>Navn</label>
+		<input disabled bind:value={patientName}/>
+		<label>Personnummer</label>
+		<input disabled value={patientId}/>
+		
+	</div>
+	
+	<!-- <button on:click={ToggleForm}>Lag ny</button> -->
+	<AddPatientForm  bind:patientName bind:patientId/>
 
 </main>
 
