0efe7eb5ca020235636240259ddb119071cd481d
Andreas <anpe@dips.no>
2021-07-14 08:46:24 +0200

    something

---
+++ src/Launch.svelte
@@ -0,0 +1,32 @@
+<script>
+    import {oauth2 as Smart} from 'fhirclient';
+	import {onMount} from 'svelte';
+
+
+    Smart.authorize({
+			clientId: 'sykepenger_dokument',
+			scope: 'launch openid fhirUser patient/*.read patient/*.write offline_access',
+            launch:	'https://localhost:9000',			
+			redirectUri: '/code',			
+            iss:'https://vt-selecta-b.dips.local/DIPS-WebAPI/HL7/FHIR-R4',
+			
+	
+		});
+
+
+// onMount(async () => {
+// 	console.log("mounter")
+// 		Smart.authorize({
+// 			clientId: 'sykepenger_dokument',
+// 			scope: 'launch openid fhirUser patient/*.read patient/*.write offline_access',
+//             //launch:	'https://localhost:9000',			
+// 			redirectUri: 'https://localhost:9000',			
+//             iss:'https://vt-selecta-b.dips.local/DIPS-WebAPI/HL7/FHIR-R4',
+			
+	
+// 		});
+// });
+
+
+
+</script>  
